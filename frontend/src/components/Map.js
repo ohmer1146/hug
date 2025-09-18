1b7d9861b226ebdbe0fa54f8e70c5812562e992c
ohmer1146 <ohmer2010@gmail.com>
2025-09-18 22:18:01 +0700

    แกไข CORS settings และ MongoDB connection

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Map = ({ lat, lng, name }) => {
-  const mapUrl = `https://www.google.com/maps/place/HomeHug+PoolVilla+Pattaya/@12.8791807,100.9058358,18.5z/data=!4m6!3m5!1s0x3102950059d21839:0xd2dd6485e54246cf!8m2!3d12.8791403!4d100.9068885!16s%2Fg%2F11lw0ljt9c?entry=ttu&g_ep=EgoyMDI1MDkxNS4wIKXMDSoASAFQAw%3D%3D`;
+  const mapUrl = `https://maps.app.goo.gl/1ogGsNeD1SGMY9WRA`;
 
   return (
     <div className="mt-6">
